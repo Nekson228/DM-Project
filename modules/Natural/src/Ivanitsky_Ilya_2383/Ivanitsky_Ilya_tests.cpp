7f9d9ccf30c1a4843410d3d52584f0661139ff60
RieJxX <i.a.ivanitski@gmail.com>
2023-11-04 20:40:30 +0300

    Revert "Ivanitsky_Ilya_2383"
    This reverts commit 5c50fabd34f37bfac969e5ab9437a18449806703.

--- modules/Natural/src/Ivanitsky_Ilya_2383/Ivanitsky_Ilya_tests.cpp
+++ modules/Natural/src/Ivanitsky_Ilya_2383/Ivanitsky_Ilya_tests.cpp
@@ -1,25 +1,31 @@
 #include <gtest/gtest.h>
 #include "../../Natural.h"
+#include <string>
+#include <sstream>
+
+static digit cmp(const Natural &a, const Natural &b){
+    return 0;
+}
 
 TEST(NaturalAdditionTest , FirstTest){
     Natural num(1000000000);
     Natural num2(123456789);
     Natural num_answer(1123456789);
-    EXPECT_TRUE(num + num2 == num_answer);
+    EXPECT_TRUE(cmp(num+num2,num_answer) == 0);
 }
 
 TEST(NaturalAdditionTest , SecondTest){
     Natural num(123456789);
     Natural num2(1234567890);
     Natural num_answer(1358024679);
-    EXPECT_TRUE(num+num2== num_answer);
+    EXPECT_TRUE(cmp(num+num2,num_answer) == 0);
 }
 
 TEST(NaturalAdditionTest , AdditionZeroTest){
     Natural num(999);
     Natural num2(0);
     Natural num_answer(999);
-    EXPECT_TRUE(num+num2 == num_answer);
+    EXPECT_TRUE(cmp(num+num2,num_answer) == 0);
 }
 
 
@@ -27,28 +33,28 @@ TEST(NaturalMultyplicationTest , FirstTest){
     Natural num(121);
     Natural num2(11);
     Natural num_answer(1331);
-    EXPECT_TRUE(num*num2 == num_answer);
+    EXPECT_TRUE(cmp(num*num2,num_answer) == 0);
 }
 
 TEST(NaturalMultyplicationTest , SecondTest){
     Natural num(1331);
     Natural num2(1331);
     Natural num_answer(1771561);
-    EXPECT_TRUE(num*num2 == num_answer);
+    EXPECT_TRUE(cmp(num*num2,num_answer) == 0);
 }
 
 TEST(NaturalMultyplicationTest , MultyplicationZeroTest){
     Natural num(11111111);
     Natural num2(0);
     Natural num_answer(0);
-    EXPECT_TRUE(num*num2 == num_answer);
+    EXPECT_TRUE(cmp(num*num2,num_answer) == 0);
 }
 
 TEST(NaturalMultyplicationTest , MultyplicationOneTest){
     Natural num(11111111);
     Natural num2(1);
     Natural num_answer(11111111);
-    EXPECT_TRUE(num*num2 == num_answer);
+    EXPECT_TRUE(cmp(num*num2,num_answer) == 0);
 }
 
 
@@ -56,14 +62,14 @@ TEST(NaturalDividingTest , FirstTest){
     Natural num(1771561);
     Natural num2(1331);
     Natural num_answer(1331);
-    EXPECT_TRUE(num/num2 == num_answer);
+    EXPECT_TRUE(cmp(num/num2,num_answer) == 0);
 }
 
 TEST(NaturalDividingTest , Secondtest){
     Natural num(10000000000);
     Natural num2(3);
     Natural num_answer(3333333333);
-    EXPECT_TRUE(num/num2 == num_answer);
+    EXPECT_TRUE(cmp(num/num2,num_answer) == 0);
 }
 
 TEST(NaturalDividingTest , DividingZeroTest){
@@ -76,7 +82,7 @@ TEST(NaturalDividingTest , DividingOneTest){
     Natural num(10000000000);
     Natural num2(1);
     Natural num_answer(10000000000);
-    EXPECT_TRUE(num/num2 == num_answer);
+    EXPECT_TRUE(cmp(num/num2,num_answer) == 0);
 }
 
 
