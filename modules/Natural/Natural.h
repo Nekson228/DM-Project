b7901729fe748d9a84f852e33a40de447ec8f736
Poglazov NIkita <74773183+Nekson228@users.noreply.github.com>
2023-11-06 00:44:23 +0300

    Update tip in Natural.h

--- modules/Natural/Natural.h
+++ modules/Natural/Natural.h
@@ -9,7 +9,7 @@ using digit = uint8_t;
 
 class Natural {
     std::vector<digit> digits_ = {}; // цифры в обратном порядке (154 == [4 5 1])
-    std::size_t n_ = -1; // номер старшей позциии
+    std::size_t n_ = -1; // номер старшей позциии (начиная с 0)
 
 public:
     Natural() = default;
