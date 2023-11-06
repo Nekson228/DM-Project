9f0ad4a582f259d91802e9280a6b22a2e7b2be63
Tsiiron <romario280404@gmail.com>
2023-11-06 14:47:19 +0300

    Tsygankov_Roman

--- modules/Polynomial/src/Tsygankov_Roman_2384/Tsygankov_Roman.cpp
+++ modules/Polynomial/src/Tsygankov_Roman_2384/Tsygankov_Roman.cpp
@@ -1,8 +1,8 @@
 #include "../../Polynomial.h"
 
-
+//Цыганков Роман гр 2384 MUL_Pxk_P - умножение мн-на на x^k
 Polynomial Polynomial::mulByXk(std::size_t k) const {
-    std::size_t degree = this->degree_ + k+ 1; // степень нового мн-на
+    std::size_t degree = this->degree_ + k; // степень нового мн-на
     std::vector <Rational> new_coeff(degree); // создаем новыый вектор с коэффицентами
     for(int i=0; i < this->degree_ + 1; i++){
         new_coeff[i] = coefficients_[i]; // присваиваем коэффиценты
@@ -13,7 +13,7 @@ Polynomial Polynomial::mulByXk(std::size_t k) const {
     }
 
     Polynomial new_coefficients_(new_coeff); // создаем мн-н с новыми коэффицентами и возвращаем его
-    new_coefficients_.degree_ = degree - 1;
+    new_coefficients_.degree_ = degree;
 
     return new_coefficients_;
 }
--- modules/Polynomial/src/Tsygankov_Roman_2384/Tsygankov_Roman_tests.cpp
+++ modules/Polynomial/src/Tsygankov_Roman_2384/Tsygankov_Roman_tests.cpp
@@ -2,15 +2,9 @@
 #include "../../Polynomial.h"
 
 TEST(mulByXk, mul_test){
-    std::vector<Rational> coef1;
-    coef1.push_back(Rational(1, 1));
-    coef1.push_back(Rational(2, 1));
-    coef1.push_back(Rational(3, 1));
+    std::vector<Rational> coef1 = {Rational(1, 1), Rational(2, 1), Rational(3, 1)};
+    std::vector<Rational> coef2 = {Rational(1, 1), Rational(2, 1), Rational(3, 1)};
 
-    std::vector<Rational> coef2;
-    coef2.push_back(Rational(1, 1));
-    coef2.push_back(Rational(2, 1));
-    coef2.push_back(Rational(3, 1));
     coef2.push_back(Rational(0, 1));
     coef2.push_back(Rational(0, 1));
 
@@ -18,4 +12,24 @@ TEST(mulByXk, mul_test){
     Polynomial g(coef2);
     EXPECT_EQ(f.mulByXk(2), g);
 
+}
+
+TEST(mulByXk, mul_test){
+    std::vector<Rational> coef1 = {Rational(1, 1), Rational(2, 1), Rational(3, 1)};
+
+    Polynomial f(coef1);
+
+    EXPECT_EQ(f.mulByXk(0), f);
+
+}
+
+TEST(mulByXk, mul_test){
+    std::vector<Rational> coef1 = {Rational(1, 1), Rational(2, 1), Rational(3, 1)};
+
+    std::vector<Rational> coef2 = {Rational(1, 1), Rational(2, 1), Rational(3, 1)};
+    coef2.push_back(Rational(0, 1));
+
+    Polynomial f(coef1);
+    Polynomial g(coef2);
+    EXPECT_EQ(f.mulByXk(1), g);
 }
