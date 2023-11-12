858dad4902a908e8be8990deac70cdeb54dfdefb
ArseniyGurkov <hurars23@gmail.com>
2023-11-12 19:15:04 +0300

    Arseniy_Gurkov

--- tests/IntegerTests/tests.cpp
+++ tests/IntegerTests/tests.cpp
@@ -20,7 +20,336 @@ TEST(GurkovArseniy, IntegerAbsTest3) {
 }
 
 TEST(Lavrenova_Yulia, isPositive1) {
+    Integer integer_1(10);#include <gtest/gtest.h>
+#include "../../modules/Integer/Integer.h"
+
+ 
+TEST(Gurkov2384_Integer_ABS_Z_N, ABS_NEGATIVE_VALUE){
+    Integer i1 = Integer(-1);
+    Integer result = i1.abs();
+    Integer expected = Integer(1);
+    ASSERT_TRUE(result == expected);
+}
+ 
+ 
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
+
+TEST(Gurkov2384_Integer_ABS_Z_N, ABS_ZERO_VALUE){
+    Integer i1 = Integer(0);
+    ASSERT_TRUE(i1.abs() == Integer(0));
+}
+
+TEST(Gurkov2384_Integer_ABS_Z_N, ABS_POSITIVE_VALUE){
+    for (int i=-100; i<=100; ++i) {
+        ASSERT_TRUE(Integer(i).abs() == Integer(std::abs(i)));
+    }
+}
+
+TEST(Lavrenova_Yulia, isPositive1) {
     Integer integer_1(10);
+    digit sign1 = integer_1.isPositive();
+    digit sign_a_1 = 2;
+
+    EXPECT_EQ(sign1, sign_a_1);
+}
+
+TEST(Lavrenova_Yulia, isPositive2) {
+    Integer integer_2(-30000);
+    digit sign_a_2 = 1;
+    digit sign2 = integer_2.isPositive();
+
+    EXPECT_EQ(sign2, sign_a_2);
+}
+
+TEST(Lavrenova_Yulia, isPositive3) {
+    digit sign_a_3 = 0;
+    Integer integer_3(0);
+    digit sign3 = integer_3.isPositive();
+
+    EXPECT_EQ(sign3, sign_a_3);
+}
+
+TEST(Lavrenova_Yulia, product1) {
+    Integer integer_1(12);
+    Integer integer_2(6);
+    Integer ans1(integer_1 * integer_2);
+    Integer ans1_test(72);
+
+    EXPECT_EQ(ans1, ans1_test);
+}
+
+TEST(Lavrenova_Yulia, product2) {
+    Integer integer_1(12);
+    Integer integer_2(-2);
+    Integer ans2(integer_1 * integer_2);
+    Integer ans2_test(-24);
+
+    EXPECT_EQ(ans2, ans2_test);
+}
+
+TEST(Lavrenova_Yulia, product3) {
+    Integer integer_1(-2);
+    Integer integer_2(-3);
+    Integer ans3(integer_1 * integer_2);
+    Integer ans3_test(6);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+
+TEST(Lavrenova_Yulia, product4) {
+    Integer integer_1(12);
+    Integer integer_2(0);
+    Integer ans4(integer_1 * integer_2);
+    Integer ans4_test(0);
+
+    EXPECT_EQ(ans4, ans4_test);
+}
+
+//TEST(IntegerTest, toNaturalNegativeTest) {
+//    Integer num(-1);
+//    EXPECT_THROW(num.toNatural(), std::invalid_argument);
+//}
+
+TEST(Sots_Katya_2384_Integer, toNaturalZeroTest) {
+    Integer num(0);
+    Natural value = num.toNatural();
+    EXPECT_EQ(value.cmp(value, Natural(0)), 0);
+}
+
+TEST(Sots_Katya_2384_Integer, toNaturalPositiveTest) {
+    Integer num(123);
+    Natural value = num.toNatural();
+    EXPECT_EQ(value.cmp(value, Natural(123)), 0);
+}
+
+TEST(Sots_Katya_2384_Integer, DivOperatorDivByZeroTest) {
+    Integer first(2);
+    Integer second(0);
+    EXPECT_THROW(first = first / second, std::invalid_argument);
+}
+
+TEST(Sots_Katya_2384_Integer, DivOperatorPosPosTest) {
+    Integer first(7);
+    Integer second(3);
+    first = first / second;
+    EXPECT_EQ(first, Integer(2));
+}
+
+TEST(Sots_Katya_2384_Integer, DivOperatorPosNegTest) {
+    Integer first(7);
+    Integer second(-3);
+    first = first / second;
+    EXPECT_EQ(first, Integer(-2));
+}
+
+TEST(Sots_Katya_2384_Integer, DivOperatorNegPosTest) {
+    Integer first(-7);
+    Integer second(3);
+    first = first / second;
+    EXPECT_EQ(first, Integer(-3));
+}
+
+TEST(Sots_Katya_2384_Integer, DivOperatorNegNegTest) {
+    Integer first(-7);
+    Integer second(-3);
+    first = first / second;
+    EXPECT_EQ(first, Integer(3));
+}
+
+TEST(TsygankovRoman, operator_sub_1) {
+    Integer integer_1(1000);
+    Integer integer_2(500);
+    Integer ans1(integer_1 - integer_2);
+    Integer ans1_test(500);
+
+    EXPECT_EQ(ans1, ans1_test);
+}
+
+TEST(TsygankovRoman, operator_sub_2) {
+    Integer integer_1(1000);
+    Integer integer_2(-500);
+    Integer ans2(integer_1 - integer_2);
+    Integer ans2_test(1500);
+
+    EXPECT_EQ(ans2, ans2_test);
+}
+
+TEST(TsygankovRoman, operator_sub_3) {
+    Integer integer_1(1000);
+    Integer integer_2(0);
+    Integer ans3(integer_1 - integer_2);
+    Integer ans3_test(1000);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+
+TEST(TsygankovRoman, operator_sub_4) {
+    Integer integer_1(-1000);
+    Integer integer_2(500);
+    Integer ans4(integer_1 - integer_2);
+    Integer ans4_test(-1500);
+
+    EXPECT_EQ(ans4, ans4_test);
+}
+
+TEST(TsygankovRoman, operator_sub_5) {
+    Integer integer_1(-1000);
+    Integer integer_2(-500);
+    Integer ans5(integer_1 - integer_2);
+    Integer ans5_test(-500);
+
+    EXPECT_EQ(ans5, ans5_test);
+}
+
+TEST(TsygankovRoman, operator_sub_6) {
+    Integer integer_1(-1000);
+    Integer integer_2(0);
+    Integer ans6(integer_1 - integer_2);
+    Integer ans6_test(-1000);
+
+    EXPECT_EQ(ans6, ans6_test);
+}
+
+TEST(TsygankovRoman, operator_sub_7) {
+    Integer integer_1(0);
+    Integer integer_2(500);
+    Integer ans7(integer_1 - integer_2);
+    Integer ans7_test(-500);
+
+    EXPECT_EQ(ans7, ans7_test);
+}
+
+TEST(TsygankovRoman, operator_sub_8) {
+    Integer integer_1(0);
+    Integer integer_2(-500);
+    Integer ans8(integer_1 - integer_2);
+    Integer ans8_test(500);
+
+    EXPECT_EQ(ans8, ans8_test);
+}
+
+TEST(TsygankovRoman, operator_sub_9) {
+    Integer integer_1(0);
+    Integer integer_2(0);
+    Integer ans9(integer_1 - integer_2);
+    Integer ans9_test(0);
+
+    EXPECT_EQ(ans9, ans9_test);
+}
+
+TEST(TsygankovRoman, constructor_1) {
+    Natural natural_(10);
+    Integer integer_(natural_);
+    Integer ans10_test(10);
+
+    EXPECT_EQ(integer_, ans10_test);
+}
+
+TEST(TsygankovRoman, constructor_2) {
+    Natural natural_(100);
+    Integer integer_(natural_);
+    Integer ans11_test(100);
+
+    EXPECT_EQ(integer_, ans11_test);
+}
+
+
+TEST(IntegerTest, negativeZeroValueTest) {
+    Integer num(0);
+    EXPECT_EQ(num.negative(), Integer(0));
+}
+
+TEST(IntegerTest, negativePositiveValueTest) {
+    Integer num(123);
+    EXPECT_EQ(num.negative(), Integer(-123));
+}
+
+TEST(IntegerTest, negativeNegativeValueTest) {
+    Integer num(-777);
+    EXPECT_EQ(num.negative(), Integer(777));
+}
+
+
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests1) {
+    Integer integer_1(2000);
+    Integer integer_2(1000);
+    Integer ans1(integer_1 + integer_2);
+    Integer ans1_test(3000);
+
+    EXPECT_EQ(ans1, ans1_test);
+}
+
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests2) {
+    Integer integer_1(-2000);
+    Integer integer_2(1000);
+    Integer ans2(integer_1 + integer_2);
+    Integer ans2_test(-1000);
+
+    EXPECT_EQ(ans2, ans2_test);
+}
+
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests3) {
+    Integer integer_1(-2000);
+    Integer integer_2(-1000);
+    Integer ans3(integer_1 + integer_2);
+    Integer ans3_test(-3000);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests4) {
+    Integer integer_1(0);
+    Integer integer_2(1000);
+    Integer ans3(integer_1 + integer_2);
+    Integer ans3_test(1000);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests5) {
+    Integer integer_1(0);
+    Integer integer_2(-1000);
+    Integer ans3(integer_1 + integer_2);
+    Integer ans3_test(-1000);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+TEST(Valeyeva2384_Integer_ADD_ZZ_Z, Add_Tests6) {
+    Integer integer_1(0);
+    Integer integer_2(0);
+    Integer ans3(integer_1 + integer_2);
+    Integer ans3_test(0);
+
+    EXPECT_EQ(ans3, ans3_test);
+}
+
+TEST(Vozgrin2382_Integer_MOD_ZZ_Z, MOD_PP) {
+    ASSERT_TRUE(Integer(21) % Integer(4) == Integer(1));
+}
+
+TEST(Vozgrin2382_Integer_MOD_ZZ_Z, MOD_NP) {
+    ASSERT_TRUE(Integer(-50) % Integer(7) == Integer(6));
+}
+
+TEST(Vozgrin2382_Integer_MOD_ZZ_Z, MOD_PN) {
+    ASSERT_TRUE(Integer(23) % Integer(-4) == Integer(3));
+}
+
+TEST(Vozgrin2382_Integer_MOD_ZZ_Z, MOD_NN) {
+    ASSERT_TRUE(Integer(-32) % Integer(-30) == Integer(28));
+}
+
+TEST(Vozgrin2382_Integer_MOD_ZZ_Z, MOD_PZero) {
+    ASSERT_THROW(Integer(10) % Integer(0),std::invalid_argument);
+}
+
+int main() {
+    ::testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
     digit sign1 = integer_1.isPositive();
     digit sign_a_1 = 2;
 
--- tests/NaturalTests/tests.cpp
+++ tests/NaturalTests/tests.cpp
@@ -125,7 +125,330 @@ TEST(IsZero_Test, test2){
     EXPECT_EQ(natural.isZero(), true);
 }
 
-TEST(Compare_test, test1){
+TEST(Compare_test, test1){#include <gtest/gtest.h>
+#include "../../modules/Natural/Natural.h"
+
+
+TEST(Borisov_IP_2383_Natural_mulByDigit, zero_mult) {
+    Natural natural(12345);
+    digit muptiplier = 0;
+    natural = natural.mulByDigit(muptiplier);
+    EXPECT_TRUE(natural.isZero());
+}
+
+TEST(Borisov_IP_2383_Natural_mulByDigit, normal) {
+    Natural natural(12345);
+    Natural check(12345 * 3);
+    digit muptiplier = 3;
+    natural = natural.mulByDigit(muptiplier);
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_mulByDigit, zero_natural) {
+    Natural natural(0);
+    digit muptiplier = 3;
+    natural = natural.mulByDigit(muptiplier);
+    EXPECT_TRUE(natural.isZero());
+}
+
+TEST(Borisov_IP_2383_Natural_subScaled, zero_other) {
+    Natural natural(12345);
+    Natural check(12345);
+    Natural natural_other(0);
+    digit k = 5;
+    natural = natural.subScaled(natural_other, k);
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_subScaled, zero_k) {
+    Natural natural(12345);
+    Natural check(12345);
+    Natural natural_other(12345);
+    digit k = 0;
+    natural = natural.subScaled(natural_other, k);
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_subScaled, zero_all) {
+    Natural natural(0);
+    Natural natural_other(0);
+    digit k = 0;
+    natural = natural.subScaled(natural_other, k);
+    EXPECT_TRUE(natural.isZero());
+}
+
+TESTBorisov_IP_2383_Natural_(subScaled, usual) {
+    Natural natural("1234567");
+    Natural check("1172842");
+    Natural natural_other(12345);
+    digit k = 5;
+    natural = natural.subScaled(natural_other, k);
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_subScaled, equal) {
+    Natural natural("1234567");
+    Natural check(1);
+    Natural natural_other("1234566");
+    digit k = 1;
+    natural = natural.subScaled(natural_other, k);
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_remains, one) {
+    Natural natural("1234567");
+    Natural natural_other(1);
+    natural = natural % natural_other;
+    EXPECT_TRUE(natural.isZero());
+}
+
+TEST(Borisov_IP_2383_Natural_remains, usual) {
+    Natural natural("1234567");
+    Natural check(67);
+    Natural natural_other(12345);
+    natural = natural % natural_other;
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Borisov_IP_2383_Natural_remains, smaller) {
+    Natural natural("1234567");
+    Natural check("1234567");
+    Natural natural_other("123456789");
+    natural = natural % natural_other;
+    EXPECT_TRUE(Natural::cmp(natural, check) == 0);
+}
+
+TEST(Damakin2384_Natural_DIV_NN_Dk, DivFirstDigitTest1){
+    Natural a = Natural(2222);
+    Natural b = Natural(111);
+    ASSERT_TRUE(a.divFirstDigit(b) == Natural(20));
+}
+
+TEST(Damakin2384_Natural_DIV_NN_Dk, DivFirstDigitTest2){
+    Natural a = Natural(111);
+    Natural b = Natural(2222);
+    ASSERT_TRUE(a.divFirstDigit(b) == Natural(20));
+}
+
+TEST(Damakin2384_Natural_DIV_NN_Dk, DivFirstDigitTest3){
+    Natural a = Natural(0);
+    Natural b = Natural(12345);
+    ASSERT_FALSE(a.divFirstDigit(b) == Natural(10000));
+}
+
+TEST(Damakin2384_Natural_DIV_NN_Dk, DivFirstDigitTest4){
+    Natural a = Natural(1);
+    Natural b = Natural(12345);
+    ASSERT_TRUE(a.divFirstDigit(b) == Natural(10000));
+}
+
+TEST(Gurkov2384_Natural_NZER_N_B, NOT_ZERO_VALUE){
+    Natural natural_1(12);
+    EXPECT_EQ(natural_1.isZero(), false);
+}
+
+TEST(Gurkov2384_Natural_NZER_N_B, ZERO_VAUE){
+    Natural natural_2(0);
+    EXPECT_EQ(natural_1.isZero(), true);
+}
+
+TEST(Gurkov2384_Natural_NZER_N_B, STANDART_CONSTRUCTOR_VALUE){
+    Natural natural_3;
+    EXPECT_EQ(natural_1.isZero(), true);
+}
+
+TEST(Gurkov2384_Natural_COM_NN_D,, FIRST_IS_BIGGER){
+    Natural a(2000);
+    Natural b(1000);
+    digit res = 2;
+    digit test = Natural::cmp(a, b);
+
+    EXPECT_EQ(res, test);
+}
+
+TEST(Gurkov2384_Natural_COM_NN_D,, FIRST_IS_SMALLER){
+    Natural a(1000);
+    Natural b(2000);
+    digit res = 1;
+    digit test = Natural::cmp(a, b);
+
+    EXPECT_EQ(res, test);
+}
+
+TEST(Gurkov2384_Natural_COM_NN_D, FIRST_EQUALS_SECOND){
+    Natural a(1000);
+    Natural b(1000);
+    digit res = 0;
+    digit test = Natural::cmp(a, b);
+
+    EXPECT_EQ(res, test);
+}
+TEST(Ilyin_2383_Natural_mulBy10k_test, degree0) {
+    Natural first(5);
+    Natural second(5);
+    Natural third;
+    third = first.mulBy10k(0);
+    EXPECT_EQ(third, second);
+}
+
+TEST(Ilyin_2383_Natural_mulBy10k_test, degree6) {
+    Natural first = Natural(5), second = Natural(5000000), third = first.mulBy10k(6);
+    EXPECT_EQ(third, second);
+}
+
+TEST(Ilyin_2383_Natural_mulBy10k_test, degree25) {
+    Natural test_object(1);
+    Natural expected_size("10000000000000000000000000");
+    EXPECT_EQ(test_object.mulBy10k(25), expected_size);
+}
+
+TEST(Ilyin_2383_Natural_lcm_test, 5and6get30) {
+    Natural first(5);
+    Natural second(6);
+    EXPECT_EQ(Natural(30), first.lcm(first, second));
+}
+
+TEST(Ilyin_2383_Natural_lcm_test, 1and1get1) {
+    Natural first(1);
+    Natural second(1);
+    EXPECT_EQ(Natural(1), first.lcm(first, second));
+}
+
+TEST(Ilyin_2383_Natural_lcm_test, getOneOfNum) {
+    Natural first("1324234234");
+    Natural second(2);
+    EXPECT_EQ(Natural("1324234234"), first.lcm(first, second));
+}
+
+TEST(Ivanitsky_Ilya_2383_ADD_NN_N , simple_addition){
+    Natural num(1000000000);
+    Natural num2(123456789);
+    Natural num_answer(1123456789);
+    EXPECT_TRUE(num + num2 == num_answer);
+}
+
+TEST(Ivanitsky_Ilya_2383_ADD_NN_N , hard_addition){
+    Natural num(123456789);
+    Natural num2(1234567890);
+    Natural num_answer(1358024679);
+    EXPECT_TRUE(num+num2== num_answer);
+}
+
+TEST(Ivanitsky_Ilya_2383_ADD_NN_N , addition_zero){
+    Natural num(111);
+    Natural num2(0);
+    Natural num_answer(111);
+    EXPECT_TRUE(Natural::cmp(num+num2,num_answer) == 0);
+}
+
+TEST(Ivanitsky_Ilya_2383_MUL_NN_N, simple_multyplication){
+    Natural num(121);
+    Natural num2(11);
+    Natural num_answer(1331);
+    EXPECT_TRUE(num*num2 == num_answer);
+}
+
+TEST(Ivanitsky_Ilya_2383_MUL_NN_N, hard_multyplication){
+    Natural num(1331);
+    Natural num2(1331);
+    Natural num_answer(1771561);
+    EXPECT_TRUE(num*num2 == num_answer);
+}
+
+TEST(Ivanitsky_Ilya_2383_MUL_NN_N, multyplication_zero){
+    Natural num(11111111);
+    Natural num2(0);
+    Natural num_answer(0);
+    EXPECT_TRUE(num*num2 == num_answer);
+}
+
+TEST(Ivanitsky_Ilya_2383_MUL_NN_N, multyplication_one){
+    Natural num(11111111);
+    Natural num2(1);
+    Natural num_answer(11111111);
+    EXPECT_TRUE(num*num2 == num_answer);
+}
+
+
+TEST(Ivanitsky_Ilya_2383_DIV_NN_N, simple_dividing){
+    Natural num(1771561);
+    Natural num2(1331);
+    Natural num_answer(1331);
+    EXPECT_TRUE(num/num2 == num_answer);
+}
+
+TEST(Ivanitsky_ILya_2383_DIV_NN_N, hard_dividing){
+    Natural num(10000000000);
+    Natural num2(3);
+    Natural num_answer(3333333333);
+    EXPECT_TRUE(num/num2 == num_answer);
+}
+
+TEST(Ivanitsky_ILya_2383_DIV_NN_N, dividing_zero){
+    Natural num(10000000000);
+    Natural num2(0);
+    EXPECT_THROW(num / num2 , std::invalid_argument);
+}
+
+TEST(Ivanitsky_ILya_2383_DIV_NN_N, dividing_one){
+    Natural num(10000000000);
+    Natural num2(1);
+    Natural num_answer(10000000000);
+    EXPECT_TRUE(num / num2 == num_answer);
+}
+
+TEST(Sots_Katya_2384_Natural, gcdZeroTest) {
+    Natural first(5);
+    Natural second(0);
+    EXPECT_EQ(first.cmp(first.gcd(first, second), first), 0);
+}
+
+TEST(Sots_Katya_2384_Natural, gcdPrimeTest) {
+    Natural first(8);
+    Natural second(5);
+    EXPECT_EQ(first.cmp(first.gcd(first, second), Natural(1)), 0);
+}
+
+TEST(Sots_Katya_2384_Natural, gcdNotPrimeTest) {
+    Natural first(16);
+    Natural second(12);
+    EXPECT_EQ(first.cmp(first.gcd(first, second), Natural(4)), 0);
+}
+
+TEST(NaturalTest, addOneZeroTest) {
+    Natural num(0);
+    EXPECT_EQ(num.addOne(), Natural(1));
+}
+
+TEST(NaturalTest, addOneNoTransitionTest) {
+    Natural num(11);
+    EXPECT_EQ(num.addOne(), Natural(12));
+}
+
+TEST(NaturalTest, addOneTransitionTest) {
+    Natural num(99);
+    EXPECT_EQ(num.addOne(), Natural(100));
+}
+
+TEST(NaturalTest, subLowerTest) {
+    Natural num(10);
+    EXPECT_THROW(num - Natural(20), std::invalid_argument);
+}
+
+TEST(NaturalTest, subEqualTest) {
+    Natural num(77);
+    EXPECT_EQ(num - Natural(77), Natural(0));
+}
+
+TEST(NaturalTest, subGreaterTest) {
+    Natural num(2248);
+    EXPECT_EQ(num - Natural(1372), Natural(876));
+}
+
+int main() {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
     Natural a(2000);
     Natural b(1000);
     digit res = 2;
